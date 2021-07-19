d15f398a00e1c197c23259bfc43ff7014286fce4
adedolapo <shayne.doly94@gmail.com>
2021-07-19 19:12:44 +0100

    finishes project

--- routes/stories.js
+++ routes/stories.js
@@ -50,23 +50,50 @@ router.get("/", ensureAuth, async (req, res) => {
 })
 
 /**
+ * @desc show single story
+ * @route GET /stories/:id
+ */
+
+router.get("/:id", ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate("user").lean()
+
+    if (!story) {
+      return res.render("error/404")
+    }
+
+    res.render("stories/show", {
+      story,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render("error/404")
+  }
+})
+
+/**
  * @desc show edit page
  * @route GET /stories/edit/:id
  */
 
 router.get("/edit/:id", ensureAuth, async (req, res) => {
-  const story = await Story.findOne({ _id: req.params.id }).lean()
+  try {
+    const story = await Story.findOne({ _id: req.params.id }).lean()
 
-  if (!story) {
-    return res.render("error/404")
-  }
+    if (!story) {
+      return res.render("error/404")
+    }
 
-  if (story.user != req.user.id) {
-    res.redirect("stories")
-  } else {
-    res.render("stories/edit", {
-      story,
-    })
+    if (story.user != req.user.id) {
+      res.redirect("stories")
+    } else {
+      res.render("stories/edit", {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    res.render("error/500")
   }
 })
 
@@ -76,21 +103,64 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
  */
 
 router.put("/:id", ensureAuth, async (req, res) => {
-  let story = await Story.findById(req.params.id)
+  try {
+    let story = await Story.findById(req.params.id)
 
-  if (!story) {
-    return res.render("error/404")
+    if (!story) {
+      return res.render("error/404")
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("stories")
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      })
+
+      res.redirect("/dashboard")
+    }
+  } catch (err) {
+    console.error(err)
+    res.render("error/500")
   }
+})
 
-  if (story.user != req.user.id) {
-    res.redirect("stories")
-  } else {
-    story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true,
-    })
+/**
+ * @desc delete story
+ * @route DELETE /stories/:id
+ */
 
+router.delete("/:id", ensureAuth, async (req, res) => {
+  try {
+    await Story.remove({ _id: req.params.id })
     res.redirect("/dashboard")
+  } catch (err) {
+    console.error(err)
+    res.render("error/404")
+  }
+})
+
+/**
+ * @desc user stories
+ * @route GET /stories/user/:userId
+ */
+
+router.get("/user/:userId", ensureAuth, async (req, res) => {
+  try {
+    let stories = await Story.find({
+      user: user.params.id,
+      status: "private",
+    })
+      .populate("user")
+      .lean()
+
+    res.render("stories/index", {
+      stories,
+    })
+  } catch (error) {
+    console.error(err)
+    res.render("error/500")
   }
 })
 
